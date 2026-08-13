cf52228af380a0f19e99235b8f5a130db7f371ac
jjardelalves <jose.jardel.alves.medeiros@ccc.ufcg.edu.br>
2026-08-13 03:25:14 -0300

    fix: remove caracter 'e' solto  no código

--- backend/main.py
+++ backend/main.py
@@ -33,7 +33,7 @@ app.include_router(permissoes_router, prefix="/api/v1")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],   e
+    allow_origins=["http://localhost:5173"],   
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
